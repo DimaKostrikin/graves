c072c951fec6eac1d1807b253514fe9239b821ad
Dmitry <dkostrikin@DESKTOP-E0SGQ3I.localdomain>
2022-12-24 03:17:43 +0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
-import { Dialog } from "@/ui/dialog";
 import styled from "styled-components";
 import { palette } from "../palette";
 import Header from "../ui/header/header";
+import { MainContainer } from "@/ui/main-container/main-container";
+import { InfoSwiper } from "@/ui/info-swiper";
 
 const PageContainer = styled.div`
   background-color: ${palette.white};
+  display: grid;
 `;
 
 const Home = () => {
   return (
     <PageContainer>
       <Header />
+      <MainContainer style={{ marginBlockStart: 160 }}>
+        <InfoSwiper />
+      </MainContainer>
     </PageContainer>
   );
 };
---
+++ src/ui/info-swiper/index.tsx
@@ -0,0 +1,70 @@
+import { palette } from "@/palette";
+import Image from "next/image";
+import styled from "styled-components";
+import image1 from "../../../public/image.jpg";
+
+const useData = () => {
+  return {
+    items: [
+      {
+        title: "Никаких забот",
+        text: "Ход выполнения работ вы можете отслеживать в личном кабинете, также все обновления будут высылаться по СМС. Изготовление, доставка, установка на нас.",
+        imageSrc: "https://geoplatforma.ru/wv/0001/1103.jpg",
+      },
+      {
+        title: "Гранит высокого качества по себестоимости",
+        text: "Наш гранит мы получаем напрямую от наших партнеров в Карелии.  Прямые поставки позволяют значительно сократить цены на памятники.",
+        imageSrc: "",
+      },
+      {
+        title: "3D визуализация",
+        text: "Перед началом работ предоставляются макеты будущего проекта. Специалисты по 3D графике учтут особые пожелания заказчика.",
+        imageSrc: "",
+      },
+    ],
+  };
+};
+
+const InfoSwiperContainer = styled.div<{ imageSrc: string }>`
+  border-radius: 20px;
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.71) 0%,
+      rgba(0, 0, 0, 0.26) 100%
+    ),
+    url(${(props) => props.imageSrc});
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding: 32px;
+  width: 100%;
+  height: 450px;
+  display: grid;
+  grid-template-rows: min-content min-content;
+  row-gap: 29px;
+  position: relative;
+`;
+
+const InfoSwiperTitle = styled.span`
+  font-family: GillSansC;
+  color: ${palette.white};
+  font-size: 64px;
+  z-index: 99;
+`;
+
+const InfoSwiperText = styled.span`
+  font-family: GillSansC;
+  color: ${palette.white};
+  font-size: 44px;
+  max-width: 70%;
+  z-index: 99;
+`;
+
+export const InfoSwiper = () => {
+  const { items } = useData();
+  return (
+    <InfoSwiperContainer imageSrc="https://geoplatforma.ru/wv/0001/1103.jpg">
+      <InfoSwiperTitle>{items[0].title}</InfoSwiperTitle>
+      <InfoSwiperText>{items[0].text}</InfoSwiperText>
+    </InfoSwiperContainer>
+  );
+};
